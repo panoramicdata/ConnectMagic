b50a405070501f0d1f7a0073bc1ab8ba1b31d700
David Bond <david.bond@panoramicdata.com>
2019-05-17 11:24:44 +0100

    Initial commit

---
+++ PanoramicData.ConnectMagic.Service/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PanoramicData.ConnectMagic.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
